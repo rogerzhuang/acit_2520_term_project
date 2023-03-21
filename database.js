666903ee64f94ee5815040e480a29b5d8a5998e5
ahammermuller <98852797+ahammermuller@users.noreply.github.com>
2023-03-21 14:16:13 -0700

    Created tags

---
+++ database.js
@@ -0,0 +1,16 @@
+let Database = {
+    cindy: {
+        reminders: [{
+            id: 1, 
+            title: "abc", 
+            description: "abcabc", 
+            completed: false,
+            tags: []
+        }]
+    },
+    alex: {
+        reminders: []
+    } 
+}
+
+module.exports = Database;
